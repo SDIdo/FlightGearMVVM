27847b7969401f4cf919f165481bd173d9753b45
SDido <bookidodo@gmail.com>
2019-04-09 00:03:37 +0300

    Uploaded all from different path due to "fatal: refusing to merge unrelated histories"

---
+++ FlightSimulator/Views/Windows/Settings.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FlightSimulator.Views.Windows
+{
+    /// <summary>
+    /// Interaction logic for Settings.xaml
+    /// </summary>
+    public partial class Settings : Window
+    {
+        public Settings()
+        {
+            InitializeComponent();
+            this.DataContext = new ViewModels.Windows.SettingsWindowViewModel(new Model.ApplicationSettingsModel());    //
+            //FlightServerIP.Text = "127";
+            //FlightInfoPort.Text = "225";
+            //FlightCommandPort.Text = "777";
+        }
+        //private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        //{
+
+        //    ViewModels.Windows.SettingsWindowViewModel settingsVM = new ViewModels.Windows.SettingsWindowViewModel(new Model.ApplicationSettingsModel());
+        //    //settingsVM.NotifyPropertyChanged("FlightServerIP");
+        //    settingsVM.FlightServerIP = FlightServerIP.Text;
+        //    MessageBox.Show($"The description is : {FlightServerIP.Text}");
+        //}
+    }
+}
